9dc6446028ab5d6f637e3f573dad675e4aa21762
carenswijaya01 <carenskurniawanwijaya@gmail.com>
2024-08-22 14:21:28 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,89 @@
+# RECONZ
+
+Automate Recon Tools for Scanning Vulnerabilities on a Single Target
+
+Reference from my mentor, thanks to [Rootbakar](https://github.com/rootbakar)!
+https://github.com/rootbakar/rb_recon_v3
+
+## Prerequisites
+
+Nuclei: https://github.com/projectdiscovery/nuclei
+
+Gau: https://github.com/lc/gau
+
+Urldedupe: https://github.com/ameenmaali/urldedupe
+
+Gf: https://github.com/tomnomnom/gf
+
+Qsreplace: https://github.com/tomnomnom/qsreplace
+
+Waybackurls: https://github.com/tomnomnom/waybackurls
+
+Gauplus: https://github.com/bp0lr/gauplus
+
+ParamSpider: https://github.com/devanshbatham/ParamSpider
+
+Httpx: https://github.com/projectdiscovery/httpx
+
+Katana: https://github.com/projectdiscovery/katana
+
+Hakrawler: https://github.com/hakluke/hakrawler
+
+Anew: https://github.com/tomnomnom/anew
+
+## Usage
+
+1. Copy and paste `.env.example` into `.env`.
+2. Configure the `.env` file.
+
+   `SAVE_DIR` = define where to save the final result.
+
+   `NUCLEI_TEMPLATE_DIR` = define where nuclei-templates located at.
+
+3. Run the following command:
+
+   ```
+   sh ./init.sh
+   ```
+
+4. Wait for the result. The result will be located in your `SAVE_DIR`.
+
+## Command Description
+
+1 = Nuclei template: /http/vulnerabilities/wordpress
+
+2 = Nuclei template: /dast/vulnerabilities (with gau, gf, qsreplace)
+
+3 = Nuclei template: /dast/vulnerabilities (with gau, qsreplace)
+
+4 = Nuclei template: /dast/vulnerabilities (with waybackurls, gf, qsreplace)
+
+5 = Nuclei template: /dast/vulnerabilities (with waybackurls, qsreplace)
+
+6 = Nuclei template: /dast/vulnerabilities (with gauplus, gf, qsreplace)
+
+7 = Nuclei template: /dast/vulnerabilities (with gauplus, qsreplace)
+
+8 = Nuclei template: /dast/vulnerabilities (with paramspider and gf)
+
+9 = Nuclei template: /dast/vulnerabilities (with paramspider)
+
+10 = Nuclei template: /dast/vulnerabilities (with katana, gf, qsreplace)
+
+11 = Nuclei template: /dast/vulnerabilities (with katana, qsreplace)
+
+12 = Nuclei template: /dast/vulnerabilities (with hakrawler, gf, qsreplace)
+
+13 = Nuclei template: /dast/vulnerabilities (with hakrawler, qsreplace)
+
+14 = Nuclei template: /http/exposures
+
+15 = Nuclei template: /http/exposed-panels
+
+16 = Nuclei template: /http/default-logins
+
+17 = Nuclei template: /default-logins
+
+## To Do
+
+- Create telegram bot for notification
---
+++ init.sh
@@ -0,0 +1,193 @@
+#!/bin/bash
+
+# Load environment variables from .env file
+if [ -f ".env" ]; then
+  export $(grep -v '^#' .env | xargs)
+fi
+
+# Global Variables
+targetUrl=""
+total_runs=17
+
+# Show Intro
+intro() {
+  echo "░▒▓███████▓▒░░▒▓████████▓▒░▒▓██████▓▒░ ░▒▓██████▓▒░░▒▓███████▓▒░░▒▓████████▓▒░ "
+  echo "░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░     ░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░      ░▒▓█▓▒░ "
+  echo "░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░     ░▒▓█▓▒░      ░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░    ░▒▓██▓▒░  "
+  echo "░▒▓███████▓▒░░▒▓██████▓▒░░▒▓█▓▒░      ░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░  ░▒▓██▓▒░    "
+  echo "░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░     ░▒▓█▓▒░      ░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░░▒▓██▓▒░      "
+  echo "░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░     ░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░        "
+  echo "░▒▓█▓▒░░▒▓█▓▒░▒▓████████▓▒░▒▓██████▓▒░ ░▒▓██████▓▒░░▒▓█▓▒░░▒▓█▓▒░▒▓████████▓▒░ "
+  echo ""
+  echo "Prerequisite: See README.md!\n"
+  echo "SAVE_DIR            : $SAVE_DIR"
+  echo "NUCLEI_TEMPLATE_DIR : $NUCLEI_TEMPLATE_DIR"
+}
+
+# Input Target URL
+getUrl() {
+  echo ""
+  echo "===== DEFINE TARGET URL ====="
+  read -p "Enter target url (without http/https): " targetUrl
+  echo ""
+
+  mkdir ./tmp-$targetUrl
+}
+
+# Execution
+run1() {
+  echo "\nNuclei template: /http/vulnerabilities/wordpress"
+  echo "$targetUrl" | nuclei -silent -t "$NUCLEI_TEMPLATE_DIR/http/vulnerabilities/wordpress" -o "./tmp-$targetUrl/wpscann-$targetUrl-nuclei.txt"
+}
+
+run2() {
+  echo "\nNuclei template: /dast/vulnerabilities (with gau, gf, qsreplace)"
+  echo "$targetUrl" | gau --subs --blacklist png,jpg,gif,jpeg,swf,woff,svg,pdf,css,webp,woff,woff2,eot,ttf,otf,mp4 | urldedupe -s | gf lfi redirect sqli-error sqli ssrf ssti xss xxe | qsreplace FUZZ | grep FUZZ | nuclei -silent -t "$NUCLEI_TEMPLATE_DIR/dast/vulnerabilities" -dast -o "./tmp-$targetUrl/result1-$targetUrl.txt"
+}
+
+run3() {
+  echo "\nNuclei template: /dast/vulnerabilities (with gau, qsreplace)"
+  echo "$targetUrl" | gau --subs --blacklist png,jpg,gif,jpeg,swf,woff,svg,pdf,css,webp,woff,woff2,eot,ttf,otf,mp4 | urldedupe -s | qsreplace FUZZ | grep FUZZ | nuclei -silent -t "$NUCLEI_TEMPLATE_DIR/dast/vulnerabilities" -dast -o "./tmp-$targetUrl/result2-$targetUrl.txt"
+}
+
+run4() {
+  echo "\nNuclei template: /dast/vulnerabilities (with waybackurls, gf, qsreplace)"
+  echo "$targetUrl" | waybackurls | urldedupe -s | gf lfi redirect sqli-error sqli ssrf ssti xss xxe | qsreplace FUZZ | grep FUZZ | nuclei -silent -t "$NUCLEI_TEMPLATE_DIR/dast/vulnerabilities" -dast -o "./tmp-$targetUrl/result3-$targetUrl.txt"
+}
+
+run5() {
+  echo "\nNuclei template: /dast/vulnerabilities (with waybackurls, qsreplace)"
+  echo "$targetUrl" | waybackurls | urldedupe -s | qsreplace FUZZ | grep FUZZ | nuclei -silent -t "$NUCLEI_TEMPLATE_DIR/dast/vulnerabilities" -dast -o "./tmp-$targetUrl/result4-$targetUrl.txt"
+}
+
+run6() {
+  echo "\nNuclei template: /dast/vulnerabilities (with gauplus, gf, qsreplace)"
+  echo "$targetUrl" | gauplus | urldedupe -s | gf lfi redirect sqli-error sqli ssrf ssti xss xxe | qsreplace FUZZ | grep FUZZ | nuclei -silent -t "$NUCLEI_TEMPLATE_DIR/dast/vulnerabilities" -dast -o "./tmp-$targetUrl/result5-$targetUrl.txt"
+}
+
+run7() {
+  echo "\nNuclei template: /dast/vulnerabilities (with gauplus, qsreplace)"
+  echo "$targetUrl" | gauplus | urldedupe -s | qsreplace FUZZ | grep FUZZ | nuclei -silent -t "$NUCLEI_TEMPLATE_DIR/dast/vulnerabilities" -dast -o "./tmp-$targetUrl/result6-$targetUrl.txt"
+}
+
+run8() {
+  echo "\nNuclei template: /dast/vulnerabilities (with paramspider and gf)"
+  paramspider -d "$targetUrl"
+  cat "./results/$targetUrl.txt" | urldedupe -s | gf lfi redirect sqli-error sqli ssrf ssti xss xxe | qsreplace FUZZ | grep FUZZ | nuclei -silent -t "$NUCLEI_TEMPLATE_DIR/dast/vulnerabilities" -dast -o "./tmp-$targetUrl/result7-$targetUrl.txt"
+}
+
+run9() {
+  echo "\nNuclei template: /dast/vulnerabilities (with paramspider)"
+  paramspider -d "$targetUrl"
+  cat "./results/$targetUrl.txt" | urldedupe -s | nuclei -silent -t "$NUCLEI_TEMPLATE_DIR/dast/vulnerabilities" -dast -o "./tmp-$targetUrl/result8-$targetUrl.txt"
+}
+
+run10() {
+  echo "\nNuclei template: /dast/vulnerabilities (with katana, gf, qsreplace)"
+  echo "$targetUrl" | httpx -silent | katana -silent | urldedupe -s | gf lfi redirect sqli-error sqli ssrf ssti xss xxe | qsreplace FUZZ | grep FUZZ | nuclei -silent -t "$NUCLEI_TEMPLATE_DIR/dast/vulnerabilities" -dast -o "./tmp-$targetUrl/result9-$targetUrl.txt"
+}
+
+run11() {
+  echo "\nNuclei template: /dast/vulnerabilities (with katana, qsreplace)"
+  echo "$targetUrl" | httpx -silent | katana -silent | urldedupe -s | qsreplace FUZZ | grep FUZZ | nuclei -silent -t "$NUCLEI_TEMPLATE_DIR/dast/vulnerabilities" -dast -o "./tmp-$targetUrl/result10-$targetUrl.txt"
+}
+
+run12() {
+  echo "\nNuclei template: /dast/vulnerabilities (with hakrawler, gf, qsreplace)"
+  echo "$targetUrl" | httpx -silent | hakrawler -subs -u | urldedupe -s | gf lfi redirect sqli-error sqli ssrf ssti xss xxe | qsreplace FUZZ | grep FUZZ | nuclei -silent -t "$NUCLEI_TEMPLATE_DIR/dast/vulnerabilities" -dast -o "./tmp-$targetUrl/result11-$targetUrl.txt"
+}
+
+run13() {
+  echo "\nNuclei template: /dast/vulnerabilities (with hakrawler, qsreplace)"
+  echo "$targetUrl" | httpx -silent | hakrawler -subs -u | urldedupe -s | qsreplace FUZZ | grep FUZZ | nuclei -silent -t "$NUCLEI_TEMPLATE_DIR/dast/vulnerabilities" -dast -o "./tmp-$targetUrl/result12-$targetUrl.txt"
+}
+
+run14() {
+  echo "\nNuclei template: /http/exposures"
+  echo "$targetUrl" | nuclei -silent -t "$NUCLEI_TEMPLATE_DIR/http/exposures" -o "./tmp-$targetUrl/exposures-$targetUrl.txt"
+}
+
+run15() {
+  echo "\nNuclei template: /http/exposed-panels"
+  echo "$targetUrl" | nuclei -silent -t "$NUCLEI_TEMPLATE_DIR/http/exposed-panels" -o "./tmp-$targetUrl/exposed-panels-$targetUrl.txt"
+}
+
+run16() {
+  echo "\nNuclei template: /http/default-logins"
+  echo "$targetUrl" | nuclei -silent -t "$NUCLEI_TEMPLATE_DIR/http/default-logins/" -o "./tmp-$targetUrl/default-logins-1-$targetUrl.txt"
+}
+
+run17() {
+  echo "\nNuclei template: /default-logins"
+  echo "$targetUrl" | nuclei -silent -t "$NUCLEI_TEMPLATE_DIR/default-logins" -o "./tmp-$targetUrl/default-logins-2-$targetUrl.txt"
+}
+
+makeResult () {
+  echo ""
+  echo "===== GENERATING RESULT ====="
+  mkdir "$SAVE_DIR/$targetUrl"
+  cd ./tmp-$targetUrl
+  cat *.txt | anew final-result-$targetUrl.txt
+  mv final-result-$targetUrl.txt "$SAVE_DIR/$targetUrl"
+  cd ..
+}
+
+removeTmp() {
+  echo ""
+  echo "===== REMOVING TMP FILES ====="
+  rm -rf ./results/$targetUrl.txt
+  rm -rf ./tmp-$targetUrl
+  echo "Done!"
+}
+
+showMenu() {
+  echo ""
+  echo "===== MAIN MENU ====="
+  echo "Target: $targetUrl"
+  echo "Choose an option:"
+  for i in $(seq 1 $total_runs); do
+    # Print options in columns
+    printf "%-15s" "$i) Run $i"
+
+    # New line every 3 options
+    if [ $(expr $i % 3) -eq 0 ]; then
+      echo ""
+    fi
+  done
+
+  if [ $(expr $total_runs % 3) -ne 0 ]; then
+    echo ""
+  fi
+
+  echo "$((total_runs + 1))) Make final result (anew)"
+  echo "$((total_runs + 2))) Run all (1-$total_runs and make final result)"
+  echo "$((total_runs + 3))) Remove temporary files"
+  echo "$((total_runs + 4))) Exit"
+}
+
+# Main script flow
+intro
+getUrl
+
+while true; do
+  showMenu
+  echo -n "Enter your choice: "
+  read choice
+
+  if [ "$choice" -ge 1 ] && [ "$choice" -le "$total_runs" ]; then
+    eval "run$choice"
+  else
+    case $choice in
+      $((total_runs + 1))) makeResult ;;
+      $((total_runs + 2)))
+        for i in $(seq 1 $total_runs); do
+          eval "run$i"
+        done
+        makeResult
+        ;;
+      $((total_runs + 3))) removeTmp ;;
+      $((total_runs + 4))) echo "Exiting..."; exit 0 ;;
+      *) echo "Invalid option. Please try again." ;;
+    esac
+  fi
+done
